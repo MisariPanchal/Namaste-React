cf555209cfcc6114e9d58a338533f1412037b194
MisariPanchal <misu220103@gmail.com>
2025-05-22 20:48:50 +0530

    EP02 - Igniting our app

--- App.js
+++ App.js
@@ -10,19 +10,19 @@
   </div>
 </div>
 */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm h1 tag."),
-    React.createElement("h2", {}, "I'm h2 tag."),
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", {key: "child1-h1"}, "I'm h1 tag."),
+    React.createElement("h2", {key: "child1-h2"}, "I'm h2 tag."),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag."),
-    React.createElement("h2", {}, "I'm h2 tag."),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", {key: "child2-h1"}, "I'm h1 tag."),
+    React.createElement("h2", {key: "child2-h2"}, "I'm h2 tag."),
   ]),
 ]);
-
-console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Life is fucking HELL...😈🔥💀
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm ~ written in C++
+- Caching ~ Faster Builds
+- Image Optimization
+- Minification 
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling ~ supports older browsers
+- Diagnostics
+- Better Error Handling and Error Suggestions
+- HTTPS
+- Tree Shaking ~ remove unused code
+- Different dev and prod bundles
